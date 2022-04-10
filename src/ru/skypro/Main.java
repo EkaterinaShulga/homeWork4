08bfbe2c5f55ba13e0746bef069ac0eae4862f21
Екатерина <shulga23@gmail.com>
2022-04-10 14:27:33 +0300

    выполнила дополнительные задачи по темам "условные операторы" и "циклы"

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -33,22 +33,22 @@ public class Main {
         } else if (year % 100 == 0 && year % 400 == 0) {
             System.out.println(year + " год является високосным.");
         } else {
-               System.out.println(year + " год не является високосным.");
+            System.out.println(year + " год не является високосным.");
 
-            }
+        }
 
         //task 4
         int deliveryDistance = 120;
-        int days = deliveryDistance/40 + 1;
-        if(deliveryDistance<=20) {
+        int days = deliveryDistance / 40 + 1;
+        if (deliveryDistance <= 20) {
             System.out.println("Потребуются сутки");
-        }else{
+        } else {
             System.out.println("Потребуется дней: " + days);
         }
 
         //task 5
-       int monthNumber = 10;
-        switch (monthNumber){
+        int monthNumber = 10;
+        switch (monthNumber) {
             case 12:
             case 1:
             case 2:
@@ -70,15 +70,108 @@ public class Main {
                 System.out.println("Осень.");
                 break;
             default:
-                System.out.println("Месяца с таким номером нет.");
+                System.out.print("Месяца с таким номером нет.");
+        }
+        System.out.println(" ");
+
+        //additional tasks
+        //tasks 6
+
+        int age = 19;
+        int salary = 58_000;
+
+        if (age >= 23) {
+            int limitOne = salary * 3;
+            if (salary >= 50000) {
+                limitOne = limitOne + ((limitOne * 120) / 100);
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом: " + limitOne + " рублей.");
+            } else if (salary >= 80000) {
+                limitOne = ((limitOne * 150) / 100) + limitOne;
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом: " + limitOne + " рублей.");
+            } else {
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом: " + limitOne + " рублей.");
+            }
+        }
+
+        if (age < 23) {
+            int limitTwo = salary * 2;
+            if (salary >= 50000) {
+                limitTwo = limitTwo + ((limitTwo * 120) / 100);
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом: " + limitTwo + " рублей.");
+            } else if (salary >= 80000) {
+                limitTwo = ((limitTwo * 150) / 100) + limitTwo;
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом: " + limitTwo + " рублей.");
+            } else {
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом: " + limitTwo + " рублей.");
+
+            }
+        }
+
+        System.out.println(" ");
+        //task7
+
+        int ageClient = 25; //возраст заемщика
+        float termOfCredit = 12f; //срок кредита
+        float salaryClient = 60_000f;//зарплата заемщка
+        float wantedSum = 330_000f; //размер займа
+        float monthlyPayment; //ежемесячный платеж
+        float monthlyPaymentMax = salaryClient / 2f;//максимльный ежемесячный платеж
+        float interestRate = 10.0f; //процентная ставка
+
+        if (ageClient < 23) {
+            interestRate = (interestRate + 1);
+            monthlyPayment = (int) Math.round((wantedSum / termOfCredit) + ((wantedSum / interestRate) / termOfCredit));
+        } else if (ageClient < 30) {
+            interestRate = (float) (interestRate + 0.5);
+            monthlyPayment = (int) Math.round((wantedSum / termOfCredit) + ((wantedSum / interestRate) / termOfCredit));
+        }
+
+        if (salaryClient > 80_000) {
+            interestRate = (float) (interestRate - 0.7);
+            monthlyPayment = (int) Math.round((wantedSum / termOfCredit) + ((wantedSum / interestRate) / termOfCredit));
+        } else {
+            monthlyPayment = (int) Math.round((wantedSum / termOfCredit) + ((wantedSum / interestRate) / termOfCredit));
+        }
+
+        if (monthlyPaymentMax > monthlyPayment) {
+            System.out.println("Максимальный платеж при ЗП " + salaryClient + " равен " + monthlyPaymentMax + " рублей. " +
+                    "Платеж по кредиту равен " + monthlyPayment + " рублей. Одобрено».");
+        } else {
+            System.out.println("Максимальный платеж при ЗП " + salaryClient + " равен " + monthlyPaymentMax + " рублей. " +
+                    "Платеж по кредиту равен " + monthlyPayment + " рублей. Отказано».");
         }
 
 
+        //task4
+        System.out.println(" ");
+        String str = ": ";
+        for (int i = 1; i < 31; i++) {
+            if (((i % 3) == 0) && ((i % 5) == 0)) {
+                System.out.println(i + str + "ping pong");
+            } else if ((i % 3) == 0) {
+                System.out.println(i + str + "ping");
+            } else if ((i % 5) == 0) {
+                System.out.println(i + str + "pong");
+            } else {
+                System.out.println(i + str);
+            }
+        }
 
 
+        System.out.println(" ");
+        //task5
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        for (int one = 1; one <= 8; ++one) {
+            int num = a + b;
+            a = b;
+            b = num;
+            System.out.print(b + " ");
         }
     }
-
+}
 
 
 
